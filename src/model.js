b0cefe1b16f14f491acffb81ac17fed6c3834215
Dmitriy Vorontsov <vorontsof@mail.ru>
2020-08-27 19:23:16 +0400

    Initial commit

---
+++ src/model.js
@@ -0,0 +1,10 @@
+export const model = [
+  {type: 'title', value: 'Test title', options: {}},
+  {type: 'text', value: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi', options: {}},
+  {type: 'textColumns', value: [
+      '1st text',
+      '2nd text',
+      '3rd text',
+      '4th text',
+    ], options: {}}
+]
